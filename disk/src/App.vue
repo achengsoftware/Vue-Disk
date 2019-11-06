8d6768f80087169838265a54f649269089dfe9fb
achengsoftware <cheng_caiyi@163.com>
2019-11-06 15:36:03 +0800

    添加文件

---
+++ disk/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <mt-popup
+    v-model="showNenu"
+    position="right"
+    pop-transition="popup-fade"
+    class="leftBar"
+    >
+    <template v-slot="description">
+        <div class="leftBarBg">
+          <mt-header title="" style="background:#343848;">
+            <router-link to="/" slot="left">
+              <mt-button icon="back" @click="showNenu=!showNenu"></mt-button>
+            </router-link>
+          </mt-header>
+          <LeftBar></LeftBar>
+        </div>
+    </template>
+    </mt-popup>
+
+    <mt-header title="Eye Disk">
+      <mt-button  slot="right" @click="showNenu=!showNenu">
+         <img src="./assets/img/home.png" height="20" width="20" slot="icon">
+      </mt-button>
+    </mt-header>
+  </div>
+</template>
+
+<script>
+  import LeftBar from "./components/leftbar/LeftBar"
+  export default {
+    components:{
+      LeftBar
+    },
+    data(){
+      return{
+        showNenu:false
+      }
+    }
+
+  }
+</script>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  color: white;
+  list-style: none;
+}
+#app{
+  font-family: '微软雅黑', 'Microsoft YaHei', Arial, Helvetica, sans-serif, '宋体';
+}
+.leftBar{
+  height: 100%;
+  width: 40%;
+  /* background-color: #26a2ff; */
+}
+  .leftBarBg{
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-color:#343848;
+  }
+</style>
---
+++ disk/src/components/leftbar/LeftBar.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <div class="avatar">
+      <img :src="head"/>
+      <span>{{name}}</span>
+    </div>
+    <ul>
+      <li class ="btngb"
+      v-for="btn in btns" 
+      :key="btn.id">
+      <a >
+        <img :src="btn.icon"/>
+        <span>{{btn.text}}</span>
+      </a>
+    </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'LeftBar',
+  props:{
+    isShowMenu:{
+      type:Boolean,
+      default:true
+    }
+  },
+  data(){
+      return{
+        head:require('../../assets/img/man.png'),
+        name:'Jom.Jonns',
+        btns:[
+          {
+            id:1,
+            icon:require('../../assets/img/home.png'),
+            text:'我的文件'
+          },
+           {
+            id:2,
+            icon:require('../../assets/img/share.png'),
+            text:'我的分享'
+          },
+           {
+            id:3,
+            icon:require('../../assets/img/logout.png'),
+            text:'退出登录'
+          },
+          {
+            id:4,
+            icon:require('../../assets/img/login.png'),
+            text:'登录'
+          },
+          {
+            id:5,
+            icon:require('../../assets/img/info.png'),
+            text:'关于'
+          }
+        ]
+      }
+  },
+  computed:{
+  },
+  methods:{
+  }
+}
+</script>
+<style lang="scss" scoped>
+.avatar{
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  margin: 0 auto;
+  margin-top: 20px;
+  margin-bottom: 60px;
+ 
+  span{
+    display: block;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+     margin: 0 auto;
+  }
+}
+
+.btngb{
+   width: 100%;
+   height: 30px;
+   font-size: 12px;
+   padding-top: 4px;
+   padding-left: 20px;
+
+   &:hover{
+     background: #000;
+   }
+  a {
+    display:inline;
+    img{
+      vertical-align: middle;
+      height: 12px;
+      width: 12px;
+    }
+    span{
+      display: inline-block;
+      height: 24px;
+      line-height: 24px;
+      text-align: center;
+
+      padding-left: 6px;
+    }
+  }
+}
+
+</style>
